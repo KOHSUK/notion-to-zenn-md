dd283a552ef0d599cd62e8e3ff1eaed89a531546
Kosuke Kihara <ashenden1965@gmail.com>
2023-11-03 07:35:57 +0000

    fix: set timezone before running test

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     include: ['./__tests__/**/*.test.ts'],
+    globalSetup: './__tests__/test-globals.ts',
   },
 });
